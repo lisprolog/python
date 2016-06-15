32770b6ea20a1748ac62f6bac4e5c3a4bc7fe939
lisprolog <peter.sattarpoor@gmx.at>
2016-06-15 14:33:11 +0100

    #5

---
+++ secretMessage.py
@@ -0,0 +1,15 @@
+#capital letters are a secret message in a random text"
+
+def find_message(text):
+    result = "";
+    for c in text:
+        if c.isupper():
+            result += c;
+    """Find a secret message"""
+    return result;
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert find_message("How are you? Eh, ok. Low or Lower? Ohhh.") == "HELLO", "hello"
+    assert find_message("hello world!") == "", "Nothing"
+    assert find_message("HELLO WORLD!!!") == "HELLOWORLD", "Capitals"
