d37230f30da473848f2327419b43551436e7c878
lisprolog <lisprolog@users.noreply.github.com>
2018-01-04 21:16:43 +0100

    Update number_radix.py

--- number_radix.py
+++ number_radix.py
@@ -1,3 +1,20 @@
+'''
+this program translates any integer base to decimal.
+examples for testing:
+    assert checkio("101", 2) == 5, "Binary"
+    assert checkio("111112", 3) == 365, "Ternary"
+    assert checkio("200", 4) == 32, "Quaternary"
+    assert checkio("101", 5) == 26, "5 base"
+    assert checkio("25", 6) == 17, "Heximal/Senary"  
+    assert checkio("112", 8) == 74, "Octal
+    assert checkio("AB", 10) == -1, "B > A > 10"
+    assert checkio("10000", 12) == 20736, "duodecimal"
+    assert checkio("AF", 16) == 175, "Hex"
+    assert checkio("20", 20) == 40, "Vigesimal"
+    assert checkio("D0", 20) == 260, "Vigesimal"
+    assert checkio("100", 20) == 400, "Vigesimal"    
+    assert checkio("Z", 36) == 35, "Hexatrigesimal"
+'''
 def checkio(str_number, radix):
     
     myList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
