c08dfc2bcff33ae867870253897c325c626a71a4
lisprolog <lisprolog@users.noreply.github.com>
2018-12-03 16:20:33 +0100

    comments correction

--- mona_captcha.py
+++ mona_captcha.py
@@ -1,5 +1,8 @@
 '''
-Your program should read the number shown in an image encoded as a binary matrix. Each digit can contain a wrong pixel, but no more than one for each digit. The space between digits is equal to one pixel (just think about "1" which is narrower than other letters, but has a width of 3 pixels).
+Your program should read the number shown in an image encoded as a binary matrix. 
+Each digit can contain a wrong pixel, but no more than one for each digit. 
+The space between digits is equal to one pixel 
+(just think about "1" which is narrower than other letters, but has a width of 3 pixels).
 
 Captcha
 
